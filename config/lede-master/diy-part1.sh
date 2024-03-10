2e1b3bf7e66ef32cb3d2308751b60c018a893528
liuyr88 <52090924+liuyr88@users.noreply.github.com>
2024-03-10 16:54:41 +0800

    Update diy-part1.sh

--- config/lede-master/diy-part1.sh
+++ config/lede-master/diy-part1.sh
@@ -12,6 +12,9 @@
 # 增加kenzok8/small插件
 sed -i '$a src-git small https://github.com/kenzok8/small.git' feeds.conf.default
 
+# 增加luci-theme-infinityfreedom插件
+sed -i '$a src-git infinityfreedom https://github.com/xiaoqingfengATGH/luci-theme-infinityfreedom.git' feeds.conf.default
+
 # other
 # rm -rf package/lean/{samba4,luci-app-samba4,luci-app-ttyd}
 
